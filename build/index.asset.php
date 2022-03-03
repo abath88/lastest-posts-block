ea9f14a9659c1175d88fd33cd16fcababc2c1b72
Albert Czarnecki <abath88@gmail.com>
2022-03-03 18:26:48 +0100

    fixed bugs

--- build/index.asset.php
+++ build/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('lodash', 'wp-block-editor', 'wp-blocks', 'wp-core-data', 'wp-data', 'wp-element', 'wp-i18n'), 'version' => '8dc56f06f7227e2098ba8f98c026c528');
+<?php return array('dependencies' => array('lodash', 'wp-block-editor', 'wp-blocks', 'wp-core-data', 'wp-data', 'wp-element', 'wp-i18n'), 'version' => '1e9e9ec783fb56b2789e2fec95994d11');
